4b87eb04a1e1570bc87194335e9735fe10af6054
Rolando Garcia <rogarcia@berkeley.edu>
2023-11-17 09:49:29 -0600

    script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,8 @@
-// This file is intentionally left blank.
+// app/static/js/script.js
+
+function openPdfViewer(pdfName) {
+    // Construct the URL for the Flask route which will serve the PDF
+    const url = `/view-pdf?name=${encodeURIComponent(pdfName)}`;
+    // Open the PDF in a new browser tab
+    window.open(url, '_blank');
+}
