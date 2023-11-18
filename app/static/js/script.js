408971ece72f7fc7c145692100ec7732605325b0
Rolando Garcia <rogarcia@berkeley.edu>
2023-11-18 11:39:54 -0600

    Update file paths and remove unused JavaScript function

--- app/static/js/script.js
+++
@@ -1,8 +0,0 @@
-// app/static/js/script.js
-
-function openPdfViewer(pdfName) {
-    // Construct the URL for the Flask route which will serve the PDF
-    const url = `/view-pdf?name=${encodeURIComponent(pdfName)}`;
-    // Open the PDF in a new browser tab
-    window.open(url, '_blank');
-}
