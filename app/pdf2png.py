ea33be8a7a4801dc47a50afe6952933d6463827f
Rolando Garcia <rogarcia@berkeley.edu>
2023-11-23 06:28:49 -0600

    Update dependencies and fix file path issues

--- app/pdf2png.py
+++ app/pdf2png.py
@@ -1,17 +1,18 @@
-import fitz  # PyMuPDF
+import fitz
 import os
+
 
 def convert_pdf_to_images(pdf_folder, output_folder):
     # Iterate through each PDF in the folder
     for pdf_file in os.listdir(pdf_folder):
-        if not pdf_file.endswith('.pdf'):
+        if not pdf_file.endswith(".pdf"):
             continue
 
         pdf_path = os.path.join(pdf_folder, pdf_file)
         pdf_name = os.path.splitext(pdf_file)[0]
 
         # Open the PDF
-        doc = fitz.open(pdf_path)
+        doc = fitz.open(pdf_path)  # type: ignore
 
         # Create a folder for this PDF's images
         pdf_output_folder = os.path.join(output_folder, pdf_name)
@@ -26,8 +27,9 @@ def convert_pdf_to_images(pdf_folder, output_folder):
 
         doc.close()
 
-if __name__ == '__main__':
-    pdf_folder = os.path.join('static', 'private', 'pdfs')
-    output_folder = os.path.join('static', 'private', 'imgs')
+
+if __name__ == "__main__":
+    pdf_folder = os.path.join("static", "private", "pdfs")
+    output_folder = os.path.join("static", "private", "imgs")
     os.makedirs(output_folder, exist_ok=True)
     convert_pdf_to_images(pdf_folder, output_folder)
