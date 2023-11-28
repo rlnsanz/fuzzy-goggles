6b430649a32c3b6c271796d56744a23405ff71c7
Rolando Garcia <rogarcia@berkeley.edu>
2023-11-28 14:35:44 -0600

    Update training_data filtering

--- train.py
+++ train.py
@@ -4,4 +4,6 @@ import flor
 
 training_data = flor.pivot("page_path", "first_page")
 training_data["page_path"] = training_data["page_path"].apply(os.path.relpath)
-print(training_data.values)
+training_data = training_data[training_data["filename"] == "infer.py"]
+training_data = training_data[training_data["tstamp"] == training_data["tstamp"].max()]
+print(training_data.head(n=len(training_data)))
